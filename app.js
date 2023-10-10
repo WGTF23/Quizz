722fc87fd1d3e60f0dbbaef46034cb6850a7baff
WGTF23 <giosagit@gmail.com>
2023-10-09 22:19:37 -0300

    Quizz

---
+++ app.js
@@ -0,0 +1,12 @@
+function VerificarResposta(){ 
+let respostaCerta
+respostaCerta = document.getElementById("alternativacerta").checked
+    
+if(respostaCerta== true){ 
+    alert("Parabéns!!!\nVocê Acertou")  
+}else{ 
+    alert("Que pena, não foi dessa vez")
+}
+}
+
+
